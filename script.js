7eb4cceca43dcff0db01727dec1bc6f838499640
xyrus8902 <130541156+xyrus8902@users.noreply.github.com>
2023-12-07 02:44:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Your JavaScript code here
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const hourHand = document.querySelector('.hour-hand');
+  const minuteHand = document.querySelector('.minute-hand');
+  const secondHand = document.querySelector('.second-hand');
+
+  const hourDeg = (hours % 12) * 30 + (minutes / 60) * 30;
+  const minuteDeg = minutes * 6 + (seconds / 60) * 6;
+  const secondDeg = seconds * 6;
+
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // Initial call to display the clock immediately
